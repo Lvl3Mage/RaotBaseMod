acd17eb385936a491b861a39802cbd0ffcc7b6a5
Lvl3Mage <lvlthreemage@gmail.com>
2022-12-23 02:42:15 +0100

    Initial commit

---
+++ ModName.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime;
+using MelonLoader;
+using UnityEngine;
+using ModNamespace;
+// using Il2CppSystem.Collections.Generic;
+using AoTNetworking.Game.Projectiles;
+using AoTNetworking.Players;
+[assembly: MelonInfo(typeof(ModName), "ModName", "0.1", "TeamName")]
+[assembly: MelonGame("RoarkInc", "raot")]
+namespace ModNamespace
+{
+    public class ModName
+    {
+    }
+}
